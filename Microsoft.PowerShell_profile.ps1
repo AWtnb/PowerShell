9bfd96ec223fa1b8318e00d3ba1f21f32dc077ea
awtnb <arikaebanataw@gmail.com>
2023-06-24 11:09:45 +0900

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -778,7 +778,7 @@ class PwshRepo {
 }
 
 function Invoke-Repository {
-    $repoDir = "C:\Personal\sync\develop\pwsh-repo"
+    $repoDir = "C:\Personal\sync\develop\PowerShell\pwsh-repo"
     if (Test-Path $repoDir) {
         $repo = [PwshRepo]::new($repoDir)
         $repo.Invoke()
