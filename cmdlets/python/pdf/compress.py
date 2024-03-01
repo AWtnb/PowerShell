c2c6f76f8d1667f793428043c921f990bcc6186b
awtnb <arikaebanataw@gmail.com>
2024-03-01 09:49:24 +0900

    update

--- cmdlets/python/pdf/compress.py
+++ cmdlets/python/pdf/compress.py
@@ -1,24 +1,26 @@
 """
-Compress pdf with PyPDF2.
+Compress pdf with PyPDF.
 encoding: utf8
 """
 
 import sys
 from pathlib import Path
 
-from PyPDF2 import PdfReader, PdfWriter
+from pypdf import PdfReader, PdfWriter
 
 
 def main(file_path: str):
-    # https://pypdf2.readthedocs.io/en/3.0.0/user/file-size.html
+    # https://pypdf.readthedocs.io/en/latest/user/file-size.html
     pdf_path = Path(file_path)
     reader = PdfReader(str(pdf_path))
     writer = PdfWriter()
     out_path = str(pdf_path.with_stem(pdf_path.stem + "_compress"))
 
     for page in reader.pages:
-        page.compress_content_streams()
         writer.add_page(page)
+
+    for page in writer.pages:
+        page.compress_content_streams()
 
     with open(str(out_path), "wb") as f:
         writer.write(f)
