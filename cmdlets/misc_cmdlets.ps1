d456a96dcc8326978c97babe2ee9f078d2ffd91f
awtnb <arikaebanataw@gmail.com>
2025-01-02 13:08:55 +0900

    update

--- cmdlets/misc_cmdlets.ps1
+++ cmdlets/misc_cmdlets.ps1
@@ -1000,3 +1000,23 @@ Set-Alias gcbf Get-ClipboardFontInfo
 function Get-VBAGistZen2han {
     Invoke-RestMethod -Uri "https://gist.githubusercontent.com/AWtnb/421c63e404c77021534082d129b92aba/raw" | Write-Output
 }
+
+function Invoke-Anchoco {
+    $gotool = $env:USERPROFILE | Join-Path -ChildPath "Personal\tools\bin\anchoco.exe"
+    if (-not (Test-Path $gotool)) {
+        "Not found: {0}" -f $gotool | Write-Host -ForegroundColor Magenta
+        $repo = "https://github.com/AWtnb/anchoco"
+        "=> Clone and build from {0}" -f $repo | Write-Host
+        return
+    }
+    $result = & $gotool ('--src={0}' -f ($env:USERPROFILE | Join-Path -ChildPath "Personal\tools\prompts.yaml"))
+    if ($LASTEXITCODE -ne 0) {
+        return
+    }
+    $result | Set-Clipboard
+    Start-Process "https://claude.ai/"
+}
+
+Set-PSReadLineKeyHandler -Key "alt+0" -BriefDescription "anchoco" -LongDescription "anchoco" -ScriptBlock {
+    Invoke-Anchoco
+}
