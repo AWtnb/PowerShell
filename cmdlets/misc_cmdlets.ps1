f4aca7b91157b8b9a655d7679c5d4ea0f8fa845f
awtnb <arikaebanataw@gmail.com>
2025-03-19 09:34:02 +0900

    add func

--- cmdlets/misc_cmdlets.ps1
+++ cmdlets/misc_cmdlets.ps1
@@ -1025,3 +1025,19 @@ Set-PSReadLineKeyHandler -Key "alt+0" -BriefDescription "anchoco" -LongDescripti
     Invoke-Anchoco
     [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
 }
+
+function Invoke-MeryBackup {
+    $src = $env:APPDATA | Join-Path -ChildPath "Mery\Mery.ini"
+    if (-not (Test-Path $src)) {
+        "'{0}' not exists." -f $src | Write-Host -ForegroundColor Magenta
+        return
+    }
+    $dest = $env:USERPROFILE | Join-Path -ChildPath "Sync\develop\app_setting\Mery\Mery.ini"
+    if (Test-Path $dest) {
+        if ((Get-Item $dest).LastWriteTime -gt (Get-Item $src).LastWriteTime) {
+            "'{0}' is newer than`n'{1}'" -f $dest, $src | Write-Host -ForegroundColor Magenta
+            return
+        }
+    }
+    Get-Item $src | Copy-Item -Destination $dest
+}
