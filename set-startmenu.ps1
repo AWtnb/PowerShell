c707d0299dc57f15dd6adbbc6b6d477a56a42e57
awtnb <arikaebanataw@gmail.com>
2025-08-15 21:49:45 +0900

    update startmenu installer

--- set-startmenu.ps1
+++ set-startmenu.ps1
@@ -1,16 +1,21 @@
-$vscode = (Get-Command "Code").Source | Split-Path -Parent | Split-Path -Parent | Join-Path -ChildPath "code.exe"
-if (Test-Path $vscode) {
-    $wsShell = New-Object -ComObject WScript.Shell
-    $startMenu = $env:APPDATA | Join-Path -ChildPath "Microsoft\Windows\Start Menu\Programs"
-    $shortcutName = "{0}-on-vscode.lnk" -f ($PSScriptRoot | Split-Path -Leaf)
-    $shortcutPath = $startMenu | Join-Path -ChildPath $shortcutName
-    $shortcut = $wsShell.CreateShortcut($shortcutPath)
-    $shortcut.TargetPath = $vscode
-    $shortcut.Arguments = '"{0}"' -f $PSScriptRoot
-    $shortcut.Save()
-    "Created shortcut on start menu: {0}" -f $shortcutPath | Write-Host -ForegroundColor Blue
+try {
+    $cmd = Get-Command "Code" -ErrorAction Stop
+    $vscode = $cmd.Source | Split-Path -Parent | Split-Path -Parent | Join-Path -ChildPath "code.exe"
+    if (Test-Path $vscode) {
+        $wsShell = New-Object -ComObject WScript.Shell
+        $startMenu = $env:APPDATA | Join-Path -ChildPath "Microsoft\Windows\Start Menu\Programs"
+        $shortcutName = "{0}-on-vscode.lnk" -f ($PSScriptRoot | Split-Path -Leaf)
+        $shortcutPath = $startMenu | Join-Path -ChildPath $shortcutName
+        $shortcut = $wsShell.CreateShortcut($shortcutPath)
+        $shortcut.TargetPath = $vscode
+        $shortcut.Arguments = '"{0}"' -f $PSScriptRoot
+        $shortcut.Save()
+        "Created shortcut on start menu: {0}" -f $shortcutPath | Write-Host -ForegroundColor Blue
+    }
+    else {
+        "Cannot find VSCode on '{0}'" -f $vscode | Write-Host -ForegroundColor Magenta
+    }
 }
-else {
-    "Cannot find VSCode on '{0}'" -f $vscode | Write-Host -ForegroundColor Magenta
+catch {
+    $_ | Write-Error
 }
-
