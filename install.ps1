b358185aa65fa44e36411b36c0bb377c4913b674
AWtnb <arikaebanataw@gmail.com>
2025-12-08 17:19:14 +0900

    force

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 New-Item $PROFILE -Force -ErrorAction SilentlyContinue
 Get-Content ($PSScriptRoot | Join-Path -ChildPath "profile.ps1") | Out-File -FilePath $PROFILE -Encoding utf8
 $d = "cmdlets"
-New-Item -Path ($PROFILE | Split-Path -Parent | Join-Path -ChildPath $d) -Value ($PSScriptRoot | Join-Path -ChildPath $d) -ItemType Junction -Confirm
+New-Item -Path ($PROFILE | Split-Path -Parent | Join-Path -ChildPath $d) -Value ($PSScriptRoot | Join-Path -ChildPath $d) -ItemType Junction -Confirm -Force
