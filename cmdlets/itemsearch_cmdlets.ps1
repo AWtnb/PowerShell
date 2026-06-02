850662cdd6ba3500f8e644e3792ca673086e3563
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-06-02 09:20:55 +0900

    remove unused

--- cmdlets/itemsearch_cmdlets.ps1
+++ cmdlets/itemsearch_cmdlets.ps1
@@ -17,6 +17,7 @@ class PsCurrentDirExtension : System.Management.Automation.IValidateSetValuesGen
         return $((Get-ChildItem -File).Extension)
     }
 }
+
 function Get-FileByExtension {
     [OutputType("System.IO.FileInfo")]
     param (
@@ -26,48 +27,4 @@ function Get-FileByExtension {
 }
 Set-Alias lsx Get-FileByExtension
 
-function lsc {
-    $clip = (Get-Clipboard | Select-Object -First 1) -replace '"'
-    if (Test-Path $clip -PathType Container) {
-        Get-ChildItem -LiteralPath $clip | Write-Output
-    }
-    else {
-        "invalid-path!" | Write-Host -ForegroundColor Magenta
-    }
-}
 
-
-class Repo {
-    [string]$path
-    Repo([string]$path) {
-        $this.path = $path
-    }
-
-    [bool]IsRemote() {
-        if (-not (Test-Path $this.path -PathType Container)) {
-            return $false
-        }
-        $g = Get-Item $this.path | Get-ChildItem -Filter ".git" -Force
-        if (-not $g) {
-            return $false
-        }
-        $grep = ($this.path | Join-Path -ChildPath ".git\config" | Get-Item | Select-String -Pattern "^\[remote .+\]")
-        return $grep.Matches.Count -gt 0
-    }
-}
-
-function Find-RemoteRepository {
-    $paths = $input | ForEach-Object {Get-Item $_.Fullname}
-    if ($paths.Count -lt 1) {
-        $paths = (Get-ChildItem -Directory).FullName
-    }
-    return $paths | Where-Object { [Repo]::new($_).IsRemote() } | ForEach-Object { Get-Item $_ }
-}
-
-function Find-LocalRepository {
-    $paths = $input | ForEach-Object {Get-Item $_.Fullname}
-    if ($paths.Count -lt 1) {
-        $paths = (Get-ChildItem -Directory).FullName
-    }
-    return $paths | Where-Object { -not [Repo]::new($_).IsRemote() } | ForEach-Object { Get-Item $_ }
-}
